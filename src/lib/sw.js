26882528f42925033777b314a40a873fae68331b
Stas <cibisovstanislav7@gmail.com>
2026-08-11 12:43:19 +0300

    upd

--- src/lib/sw.js
+++ src/lib/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "pulseguard-cache-v1";
-const APP_SHELL = ["/", "/manifest.webmanifest", "/icon.svg"];
+const APP_SHELL = ["/", "/manifest.webmanifest", "/icon.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
